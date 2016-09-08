67e729ac6cb13ec3ef6905a117aee7725c79cac4
Ryan Ip <ryan.ip@playtasy.com>
2016-09-08 21:32:18 +0800

    Update

---
+++ test/sms.js
@@ -0,0 +1,32 @@
+'use strict';
+
+var chai = require('chai');
+var SMSClient = require('../lib/sms-client');
+
+var expect = chai.expect;
+
+var APIKEY = 'test';
+var APISECRET = 'test';
+
+describe('SMSClient Test Suite', function() {
+
+	describe('#authenticate', function() {
+
+		it('should fail to authenticate and return error 401', function(done) {
+			SMSClient.authenticate(null, null, function(err) {
+				expect(err).to.be.not.null;
+				expect(err).to.have.property('statusCode', 401);
+				done();
+			});
+		});
+
+		it('should authenticate successfully with test account', function(done) {
+			SMSClient.authenticate(APIKEY, APISECRET, function(err) {
+				expect(err).to.be.null;
+				done();
+			});
+		});
+
+	});
+
+});
